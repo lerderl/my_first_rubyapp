767483ff494eb5f23e8cdbea530cac600c3ecb35
lerderl <josepholukunle1107@gmail.com>
2025-05-23 09:19:01 +0100

    fix: updated failing tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -16,9 +16,17 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create article" do
-    assert_difference("Article.count") do
-      post articles_url, params: { article: { content: @article.content, title: @article.title } }
+    assert_difference("Article.count", 1) do
+      post articles_url, params: {
+        article: {
+          title: "Unique Title #{rand(1000)}", # Ensures uniqueness
+          content: "Valid content that isn't too long and contains no bad words"
+        }
+      }
     end
+    # assert_difference("Article.count") do
+    #   post articles_url, params: { article: { content: @article.content, title: @article.title } }
+    # end
 
     assert_redirected_to article_url(Article.last)
   end
@@ -34,7 +42,13 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update article" do
-    patch article_url(@article), params: { article: { content: @article.content, title: @article.title } }
+    patch article_url(@article), params: {
+      article: {
+        title: "Updated Title #{rand(1000)}", # Ensures uniqueness
+        content: "Short and clean content" # Avoids validation errors
+      }
+    }
+    # patch article_url(@article), params: { article: { content: @article.content, title: @article.title } }
     assert_redirected_to article_url(@article)
   end
 
